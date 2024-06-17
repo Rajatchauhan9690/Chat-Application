b5226642bcbd4fbb8d41602f72c8f3aff3bdf8f8
Rajat Chauhan <rajatchauhan9690@gmail.com>
2024-06-18 00:20:34 +0530

    update signup

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast"
+import {BASE_URL} from "..";
 function Signup() {
   const [user, setUser] = useState({
     fullName: "",
@@ -12,53 +15,81 @@ function Signup() {
     setUser({ ...user, gender });
   };
   const onSubmitHandler = (e) => {
-    e.preventDefault();
-    console.log(user);
-  };
+    e.preventDefault()
+     try {
+      const res = await axios.post(`${BASE_URL}/api/v1/user/register`, user, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      });
+      if (res.data.success) {
+        navigate("/login");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.log(error);
+    }
+      setUser({
+      fullName: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+      gender: "",
+    })
+  }
+  
   return (
     <div className="min-w-96 mx-auto">
-      <div className="w-full p-4 rounded-xl shadow-md bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
-        <h1 className="text-center text-3xl text-white font-bold">Signup</h1>
+      <div className="w-full p-4 rounded-xl shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100">
+        <h1 className="text-center text-3xl font-bold text-white ">Signup</h1>
         <form onSubmit={onSubmitHandler} action="">
           <div>
             <label className="label p-2">
-              <span className="text-based label-text font-bold">Full Name</span>
+              <span className="text-based label-text font-bold text-white text-lg">
+                Full Name
+              </span>
             </label>
             <input
               value={user.fullName}
               onChange={(e) => setUser({ ...user, fullName: e.target.value })}
-              className="w-full input  input-bordered-10 bg-black"
+              className="w-full input  input-bordered-10 bg-black text-white"
               type="text"
               placeholder="FullName"
             />
           </div>
           <div>
             <label className="label p-2">
-              <span className="text-based label-text font-bold">User Name</span>
+              <span className="text-based label-text font-bold text-white text-lg">
+                User Name
+              </span>
             </label>
             <input
               value={user.username}
               onChange={(e) => setUser({ ...user, username: e.target.value })}
-              className="w-full input  input-bordered-10 bg-black"
+              className="w-full input  input-bordered-10 bg-black text-white"
               type="text"
               placeholder="UserName"
             />
           </div>
           <div>
             <label className="label p-2">
-              <span className="text-based label-text font-bold">Password</span>
+              <span className="text-based label-text font-bold text-white text-lg">
+                Password
+              </span>
             </label>
             <input
               value={user.password}
               onChange={(e) => setUser({ ...user, password: e.target.value })}
-              className="w-full input  input-bordered-10 bg-black"
+              className="w-full input  input-bordered-10 bg-black text-white"
               type="text"
               placeholder="Password"
             />
           </div>
           <div>
             <label className="label p-2">
-              <span className="text-based label-text font-bold">
+              <span className="text-based label-text font-bold text-white text-lg">
                 Confirm Password
               </span>
             </label>
@@ -67,12 +98,12 @@ function Signup() {
               onChange={(e) =>
                 setUser({ ...user, confirmPassword: e.target.value })
               }
-              className="w-full input  input-bordered-10 bg-black"
+              className="w-full input  input-bordered-10 bg-black text-white"
               type="text"
               placeholder="ConfirmPassword"
             />
           </div>
-          <div className="flex items-center my-4 text-white font-bold">
+          <div className="flex items-center my-4  font-bold text-white text-lg">
             <div className="flex items-center mx-2">
               <p>Male</p>
               <input
@@ -95,10 +126,10 @@ function Signup() {
             </div>
           </div>
           <div className="w-full mx-auto flex items-center justify-center">
-            <p className="text-white my-2">
-              Already have an account ,Please Login
+            <p className="text-white text-lg font-bold my-2">
+              Already have an account ?
             </p>
-            <Link to="/login"></Link>
+            <Link to="/login">Login</Link>
           </div>
           <div className="flex items-center justify-center ">
             <button
