6c6c12e9f68b4deeb5b48414d7e889befd2b4029
Rajat Chauhan <rajatchauhan9690@gmail.com>
2024-06-17 14:54:24 +0530

    make bioplate of 3 components home,signup,login

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signup from "./components/Signup";
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/register",
+    element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 function App() {
   return (
-    <>
-      <h1 className="text-center font-bold"> helo india</h1>
-    </>
+    <div className="p-4 h-screen flex items-center">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
---
+++ Frontend/src/components/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div>
+      Register
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Signup;
