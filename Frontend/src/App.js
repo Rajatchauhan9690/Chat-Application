506c61e15a7e7e49fd2414683c02f0e9eaed82f0
Rajat Chauhan <rajatchauhan9690@gmail.com>
2024-06-18 20:51:59 +0530

    complete frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,22 @@
-import "./App.css";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Signup from "./components/Signup";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import io from "socket.io-client";
+import { setSocket } from "./redux/socketSlice";
+import { setOnlineUsers } from "./redux/userSlice";
+import { BASE_URL } from ".";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
   {
-    path: "/register",
+    path: "/signup",
     element: <Signup />,
   },
   {
@@ -18,7 +24,33 @@ const router = createBrowserRouter([
     element: <Login />,
   },
 ]);
+
 function App() {
+  const { authUser } = useSelector((store) => store.user);
+  const { socket } = useSelector((store) => store.socket);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (authUser) {
+      const socketio = io(`${BASE_URL}`, {
+        query: {
+          userId: authUser._id,
+        },
+      });
+      dispatch(setSocket(socketio));
+
+      socketio?.on("getOnlineUsers", (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
+      });
+      return () => socketio.close();
+    } else {
+      if (socket) {
+        socket.close();
+        dispatch(setSocket(null));
+      }
+    }
+  }, [authUser]);
+
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <RouterProvider router={router} />
